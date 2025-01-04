5e33f40dc5ce145e1cefa4f745f0d1f723457c03
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2025-01-04 17:20:17 +0530

    Add files via upload

---
+++ CaseStudy.java
@@ -0,0 +1,21 @@
+package com.logicalstatement;
+
+import java.util.Scanner;
+
+public class CaseStudy {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Your Marks ");
+		int marks = sc.nextInt();
+		
+		if(marks >50 ) {
+			System.out.println("Congratulations! You are the Eligable for the next Exam man");
+		}
+		else {
+			System.out.println("Sorry, you are not eligable for the next Exam");
+		}
+
+	}
+
+}
