5d2b298ad36b2f26f1f961a74db7ed74d9a23053
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2025-01-04 17:25:46 +0530

    Add files via upload

---
+++ SwappingWith.java
@@ -0,0 +1,18 @@
+package com.logicalstatement;
+
+public class SwappingWith {
+  public static void main(String[] args) {
+	
+	  
+	  int a = 30;
+	  int b = 80;
+	  int c = 0;
+	  
+	  c = a;
+	  a = b;
+	  b = c;
+	  
+	  System.out.println("A numbers is  = "+a);
+	  System.out.println("B numbers is = "+b);
+}
+}
