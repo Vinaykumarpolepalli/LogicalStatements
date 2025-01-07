da66048cb87264696e9dceb06147ef7afe6abc76
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2025-01-07 17:53:46 +0530

    Add files via upload

---
+++ TopEvenNumber.java
@@ -0,0 +1,15 @@
+package com.logicalstatement;
+
+public class TopEvenNumber {
+
+	public static void main(String[] args) {
+	for(int i =0;i<=10;i++) {
+		if(i %2==0) {
+			System.out.println(i+"even");
+		}
+		
+	}
+
+	}
+
+}
