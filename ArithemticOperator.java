0abcfa77e71ce8e4e203ffc0e0b89afbeb15ccfa
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2025-01-07 17:51:21 +0530

    Add files via upload

---
+++ ArithemticOperator.java
@@ -0,0 +1,29 @@
+package com.logicalstatement;
+
+public class ArithemticOperator {
+
+	public static void main(String[] args) {
+	    int a = 10;
+	    int b = 20;
+	     
+	    Double d = 100.0;
+	    Double d1 = 200.0;
+	    
+	    
+	    System.out.println(a+b);
+	    System.out.println(a-b);
+	    System.out.println(a*b);
+	    System.out.println(a/b);
+	    System.out.println(a%b);
+	    
+	    System.out.println("**********************************");
+	    System.out.println(d+d1);
+	    System.out.println(d-d1);
+	    System.out.println(d*d1);
+	    System.out.println(d/d1);
+	    System.out.println(a%d1);
+	    
+
+	}
+
+}
