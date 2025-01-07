fd813e67afcf2d702aca08ff2124ef633e3e01e5
Vinaykumarpolepalli <vinaykumarpolepalli@gmail.com>
2025-01-07 17:53:31 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,12 @@
+package com.logicalstatement;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		int a = 20 ;
+		String s =(a%2 ==0)? "Even":"odd";
+		System.out.println(s);
+
+	}
+
+}
